2fb7a1935cd9f912b115d9f78e9530ab758e6697
Jack DuPlessis <jduplessis294@gmail.com>
2017-10-28 18:53:41 -0400

    Update tests

--- test/test.js
+++ test/test.js
@@ -4,23 +4,59 @@ const assert = require('assert')
 
 describe('githubSVG', function () {
   describe('#createSVG', function () {
-    it('should replace commits with correct number', function () {
-      const correctSVG = '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="300" height="200"> <rect width="300" height="200" fill="#FFFFFF" stroke="#000000" stroke-width="6px"/> <text x="5" y="19"> Commits: 13 </text></svg>'
-      assert.equal(githubSVG.createSVG(13), correctSVG)
+    it('should replace widen badge for longer repos', function () {
+      assert.ok(githubSVG.createSVG(
+        {repo: 'asdfasdfasdfasdf',
+          user:'a',
+          stars: '2',
+          forks: '3',
+          contributors: '5',
+          watchers: '0',
+          commits: '45'
+        }).indexOf('310') !== -1)
+    })
+    it('should replace widen badge for bigger stats', function () {
+      assert.ok(githubSVG.createSVG(
+        {repo: 'farts',
+          user:'a',
+          stars: '2',
+          forks: '3',
+          contributors: '5',
+          watchers: '5',
+          commits: '45'
+        }).indexOf('300') !== -1)
+      assert.ok(githubSVG.createSVG(
+        {repo: 'farts',
+          user:'a',
+          stars: '2',
+          forks: '3',
+          contributors: '555,555,555,555',
+          watchers: '0',
+          commits: '45'
+        }).indexOf('324') !== -1)
+      assert.ok(githubSVG.createSVG(
+        {repo: 'farts',
+          user:'a',
+          stars: '2',
+          forks: '3',
+          contributors: '5',
+          watchers: '555555555555',
+          commits: '45'
+        }).indexOf('303') !== -1)
     })
   })
 })
 describe('githubInfo', function () {
   describe('#getInfo', function () {
     it('should return information for a github repo', function (done) {
-      githubInfo.getInfo('samdup123', 'maze', function (err, body) {
-        console.log(body)
+      githubInfo.getInfo('samdup123', 'Collatz', function (err, body) {
+        assert.ok(body !== undefined)
         done()
       })
     })
     it('should throw an error for invalid repo', function (done) {
       githubInfo.getInfo('jjjjjj', 'jjjjjjj', function (err, body) {
-        console.log(err)
+        assert.ok(err !== undefined)
         done()
       })
     })
